b4bdd3494809cc39ccf0bd4212e50dbb4f7e01c7
mekelangelo <mekelangelo@proton.me>
2025-10-27 20:57:41 +0200

    Add SecureCounterAddresses.ts

---
+++ client/abi/SecureCounterAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const SecureCounterAddresses = {
+  "11155111": { address: "0x390E982df28f58ff993D2D3382F4dd68D1d9Ee43", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", chainId: 31337, chainName: "hardhat" },
+};
